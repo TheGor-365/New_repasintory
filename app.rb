b5983ddedcc842a4a51487e21174816d94322261
Gor <7495500@gmail.com>
2020-02-28 02:21:52 +0300

    Visiting form import, login form, login validation

--- app.rb
+++ app.rb
@@ -2,43 +2,40 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 
-configure do
-  enable :sessions
+get '/about' do
+  erb :about
 end
 
-helpers do
-  def username
-    session[:identity] ? session[:identity] : 'Sign in'
-  end
+get '/visit' do
+  erb :visit
 end
 
-before '/secure/*' do
-  unless session[:identity]
-    session[:previous_url] = request.path
-    @error = 'Sorry, you need to be logged in to visit ' + request.path
-    halt erb(:login_form)
-  end
+get '/contacts' do
+  erb :contacts
 end
 
 get '/' do
-  erb 'You must sign for <a href="/secure/place">visit</a>?'
+  erb :home
 end
 
 get '/login/form' do
   erb :login_form
 end
 
-post '/login/attempt' do
-  session[:identity] = params['username']
-  where_user_came_from = session[:previous_url] || '/'
-  redirect to where_user_came_from
+post '/info.txt' do
+  @login = params[:login]
+  @password = params[:password]
+
+  if @login != "Gor" && @password != "123"
+    @error = "Wrong login or password"
+    erb :login_form
+  else
+    erb "<a href='info.txt'></a>"
+  end
+
 end
 
 get '/logout' do
   session.delete(:identity)
   erb "<div class='alert alert-message'>Logged out</div>"
-end
-
-get '/secure/place' do
-  erb 'This is a secret place that only <%=session[:identity]%> has access to!'
 end
