b0b033a5ad03e6e0a0e533dfdc540dadcedc14db
Gor <7495500@gmail.com>
2020-02-28 14:50:31 +0300

    Add visiting form and visiting validation

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/about' do
+  @error = 'This is error!'
   erb :about
 end
 
@@ -22,11 +23,17 @@ get '/login/form' do
   erb :login_form
 end
 
+get '/logout' do
+  session.delete(:identity)
+  erb "<div class='alert alert-message'>Logged out</div>"
+end
+
 post '/info.txt' do
   @login = params[:login]
   @password = params[:password]
 
   if @login != "Gor" && @password != "123"
+
     @error = "Wrong login or password"
     erb :login_form
   else
@@ -35,7 +42,32 @@ post '/info.txt' do
 
 end
 
-get '/logout' do
-  session.delete(:identity)
-  erb "<div class='alert alert-message'>Logged out</div>"
+post '/visit' do
+
+  @username = params[:username]
+  @phone = params[:phone]
+  @date_time = params[:date_time]
+  @barber = params[:barber]
+  @color = params[:color]
+
+  visit_form_alerts = {
+    :username => 'Input your name',
+    :phone => 'Input your phone',
+    :date_time => 'Input your visiting date and time'
+  }
+
+  visit_form_alerts.each do |key, value|
+
+    if params[key] == ''
+      @error = visit_form_alerts[key]
+      return erb :visit
+    end
+  end
+
+  # info = File.open '/info.txt', 'a'
+  # info.write "Date and time: #{@date_time} | Phone: #{@phone} | Username: #{@username} Barber: #{@barber} | Color: #{@color}\n"
+  # info.close
+
+  @message = "Your info is sended:\n\nDate and time: #{@date_time} | Phone: #{@phone} | Username: #{@username} | Barber: #{@barber} | Color: #{@color}"
+  erb :visit
 end
